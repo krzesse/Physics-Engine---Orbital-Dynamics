7734bb10d211ae250ad89dd713a4f94297892631
krzesse <154025805+krzesse@users.noreply.github.com>
2025-04-28 20:02:00 +0200

    Update Adams_Bashforth_Solver.java
    fixed AB4 to compute values for single stepsize h at t

--- src/Physics_Engine/SolarSystem/Adams_Bashforth_Solver.java
+++ src/Physics_Engine/SolarSystem/Adams_Bashforth_Solver.java
@@ -12,52 +12,52 @@ public class Adams_Bashforth_Solver {
     }
 
     // the thing is that X has Three value for each dimensions and three of each for each t . DOes this take into account ??
-    public double[] AB4(double h, double[] t, double[] x, ODEFunction function){
-        double[][] history = new double[t.length][x.length];
+    public double[] AB4(double h, double t, double[] x, double[][] history, ODEFunction function){
 
         //Bootstraping first using classical fourth order Runge-Kuntta method
-        for (int i = 0; i < 3; i++) {
-            double[] k1 = function.computeDerivative(x, t[i]);
+        if (t/h < 3) {
+            double[] k1 = function.computeDerivative(x, t);
             double[] xTemp = new double[x.length];
             // k2 = f(x + h/2 * k1, t + h/2)
             for (int j = 0; j < x.length; j++) {
                 xTemp[j] = x[j] + h / 2.0 * k1[j];
             }
-            double[] k2 = function.computeDerivative(xTemp, t[i] + h / 2);
+            double[] k2 = function.computeDerivative(xTemp, t + h / 2);
             // k3 = f(x + h/2 * k2, t + h/2)
             for (int j = 0; j < x.length; j++) {
                 xTemp[j] = x[j] + h / 2.0 * k2[j];
             }
-            double[] k3 = function.computeDerivative(xTemp, t[i] + h / 2);
+            double[] k3 = function.computeDerivative(xTemp, t + h / 2);
             // k4 = f(x + h * k3, t + h)
             for (int j = 0; j < x.length; j++) {
                 xTemp[j] = x[j] + h * k3[j];
             }
-            double[] k4 = function.computeDerivative(xTemp, t[i] + h);
+            double[] k4 = function.computeDerivative(xTemp, t + h);
             // Update the state using the RK4 formula
             for (int j = 0; j < x.length; j++) {
                 x[j] = x[j] + h / 6.0 * (k1[j] + 2 * k2[j] + 2 * k3[j] + k4[j]);
                 //Store x values after each time step
-                history[i][j] = x[j];
+                history[t/h][j] = x[j];
             }
             System.out.println("Step : " + i + " | t = " + t[i] + " | x = " + Arrays.toString(x));
+            return x;
         }
+        
 
         //Using AB4 method onward when we have previous 4 derivatives
-        for(int k = 4; k < t.length; k++){
+        else{
             for (int j = 0; j < x.length; j++) {
                 //x_j = x_j-1 + h/24 * (55 * f(x_j-1, t_j-1) - 59 * f(x_j-2, t_j-2) + 37 * f(x_j-3, t_j-3- 9 * f(x_j-4, t_j-4))
                 x[j] = x[j] + h / 24.0 * (
-                        55 * function.computeDerivative(history[k - 1], t[k - 1])[j]
-                                - 59 * function.computeDerivative(history[k - 2], t[k - 2])[j]
-                                + 37 * function.computeDerivative(history[k - 3], t[k - 3])[j]
-                                -  9 * function.computeDerivative(history[k - 4], t[k - 4])[j]
+                        55 * function.computeDerivative(history[t/h - h], t - h)[j]
+                                - 59 * function.computeDerivative(history[t/h -2h], t-2h)[j]
+                                + 37 * function.computeDerivative(history[t/h - 3h], t-3h)[j]
+                                -  9 * function.computeDerivative(history[t/h - 4h], t-4h)[j]
                 );
 
-                history[k][j] = x[j];
+                history[t/h][j] = x[j];
             }
         }
-
 
         //Return the final stage, approximated value x at final time step
         return x;
